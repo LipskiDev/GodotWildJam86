9d689ce4e429151c3f1c03e5ba2e0641badb4001
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-10-11 19:09:51 +0200

    besseres movement

--- project/entities/player/player.gd
+++ project/entities/player/player.gd
@@ -2,31 +2,50 @@ class_name Player
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 6.0
+const JUMP_VELOCITY = 7.0
+const COYOTE_TIME = 0.2 	# BUG: wenn man schnell jump drückt kann man double jump machen
+
+
+var movement_force: float = 0.09 # Kraft der aktuellen input eingebe
+
+var on_floor: bool = true
+var coyote_timer: float = 0.0
+
+
+@onready var rotatable_objects: Node3D = %RotatableObjects
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add the gravity. and coyote time
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		coyote_timer += delta
+		if coyote_timer >= COYOTE_TIME:
+			on_floor = false
+			coyote_timer = 0.0
+	else:
+		on_floor = true
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and on_floor:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		var wish_dir: Vector2
+		wish_dir.x = direction.x * SPEED
+		wish_dir.y = direction.z * SPEED
+		
+		velocity.x = velocity.x * (1.0 - movement_force) + wish_dir.x * movement_force
+		velocity.z = velocity.z * (1.0 - movement_force) + wish_dir.y * movement_force
 		
 		# Rotate mesh to face movement direction
-		$Meshes.rotation.y = atan2(direction.x, direction.z) + PI
+		rotatable_objects.rotation.y = atan2(direction.x, direction.z) + PI
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED * 0.1)
+		velocity.z = move_toward(velocity.z, 0, SPEED * 0.1)
 
 	move_and_slide()
