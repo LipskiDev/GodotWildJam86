9d2e8979e76f55c66bfe117e924a4d8d542b7d8e
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-10-13 19:17:52 +0200

    double jump und gridmap (sieht kacke aus)

--- project/entities/player/player.gd
+++ project/entities/player/player.gd
@@ -7,11 +7,15 @@ const JUMP_VELOCITY = 7.0
 const COYOTE_TIME = 0.2 	# BUG: wenn man schnell jump drückt kann man double jump machen
 
 
+@export var max_jumps: int = 2
+
+
 var movement_force: float = 0.09 # Kraft der aktuellen input eingebe
 
 var on_floor: bool = true
 var coyote_timer: float = 0.0
 
+var jumps: int = 0
 
 @onready var rotatable_objects: Node3D = %RotatableObjects
 
@@ -26,10 +30,15 @@ func _physics_process(delta: float) -> void:
 			coyote_timer = 0.0
 	else:
 		on_floor = true
+		jumps = max_jumps
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and on_floor:
+	# Handle jump. and double jump
+	if Input.is_action_just_pressed("jump") and (on_floor or jumps > 0):
+		if jumps == max_jumps and not on_floor:
+			jumps -= 1
 		velocity.y = JUMP_VELOCITY
+		jumps -= 1
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
