9e41daaa70e4e7e96261f6ca982bc50dd35f8dff
hanskraschewski <hans.kraschewski@mailbox.tu-dresden.de>
2025-10-14 22:30:41 +0200

    squash animation

---
+++ project/entities/enemies/states/squash.gd
@@ -0,0 +1,21 @@
+class_name Squash
+extends State
+
+
+@export var enemy: CharacterBody3D
+@export var animation_tree: AnimationTree
+var player: CharacterBody3D
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
+
+
+func update(delta: float) -> void:
+	pass
+
+
+func physics_update(delta: float) -> void:
+	pass
