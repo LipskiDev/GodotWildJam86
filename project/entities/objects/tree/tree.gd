0aca5d20792b56eab6f566be9a84624eac7f125f
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-10-14 21:08:41 +0200

    bäume waren weg omegalul

---
+++ project/entities/objects/tree/tree.gd
@@ -0,0 +1,24 @@
+extends StaticBody3D
+
+
+@onready var leafs: MeshInstance3D = $Leafs
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func take_damage(_amount: int) -> void:
+	$CPUParticles3D.emitting = true
+	
+	var radius = leafs.mesh.radius
+	
+	var tween = get_tree().create_tween()
+	$Leafs.mesh.radius = radius * 0.5
+	tween.tween_property($Leafs.mesh, "radius", radius, 0.5).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
