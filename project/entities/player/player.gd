1111ad37ac7af7a3a6efb957dfe0af2bd79ae586
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-10-19 20:35:50 +0200

    balloon gegner is da

--- project/entities/player/player.gd
+++ project/entities/player/player.gd
@@ -121,11 +121,12 @@ func take_damage(_amount: int) -> void:
 	if health <= 0:
 		Globals.player_died.emit()
 		show_you_died_screen()
-		
+
+
 func show_you_died_screen() -> void:
 	$AnimationPlayer.play("youdied")
-		
-	
+
+
 func player_died() -> void:
 	if Globals.last_bonfire_scene != "":
 		Globals.player_just_died = true
