2e7cdac16fac669ebe515be9b100965c10370c4e
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-10-19 17:50:53 +0200

    cactus

---
+++ project/entities/player/stick.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var attack_area: Area3D = $Armature/Skeleton3D/BoneAttachment3D/AttackArea
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("attack") and Globals.current_mask == 1:
+		animation_player.play("Attack")
+		attack_area.monitoring = true
+		
+		await get_tree().create_timer(0.6).timeout
+		
+		attack_area.monitoring = false
+
+
+func _on_attack_area_body_entered(body: Node3D) -> void:
+	if body.has_method("take_damage_stick"):
+		body.take_damage_stick(1)
