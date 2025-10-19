bd0288d98b0d8295bad1217961e8773acca73703
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-10-19 15:15:22 +0200

    masken switch

--- project/globals/globals.gd
+++ project/globals/globals.gd
@@ -24,10 +24,3 @@ var player_just_died: bool = false
 var next_door_id: int = -1
 
 var current_mask: int = -1
-
-@warning_ignore("unused_signal")
-signal teleport_player_to(pos: Vector3)
-
-var is_bonfire_lit: Array[bool] = [false, false, false, false, false, false]
-
-var last_bonfire_scene: String = ""
