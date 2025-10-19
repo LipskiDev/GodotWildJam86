045e7a87e7b68f927f981f2fe14cc8030efeb66c
HPLaptop <Hans.Kraschewski@mailbox.tu-dresden.de>
2025-10-19 13:28:41 +0200

    cactus mostly done

--- project/entities/enemies/Cactus/cactus.gd
+++ project/entities/enemies/Cactus/cactus.gd
@@ -1,9 +1,11 @@
 extends CharacterBody3D
 
-@export var shoot_speed: int = 3
+@export var shoot_cooldown: int = 3
+@export var contact_hit_cooldown: int = 3
 var health: int = 100
 var player: Player = null
 var player_locked: bool = false
+var player_touching: bool = true
 var tween: Tween
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -11,6 +13,8 @@ var tween: Tween
 func _ready() -> void:
 	$EyeLeft.light_energy = 0.0
 	$EyeRight.light_energy = 0.0
+	$ShootTimer.wait_time = shoot_cooldown
+	$ContactHitTimer.wait_time = contact_hit_cooldown
 	
 func _process(delta: float) -> void:
 	if player_locked:
@@ -31,12 +35,28 @@ func attack():
 	
 func take_damage(dmg: int):
 	print("cactus: takes damage")
-	health -= dmg;
+	if animation_player.current_animation == "die":
+		return
+	
+	health -= dmg
 	if health <= 0:
 		die()
+	
+	
+	if tween:
+		tween.kill()
+	
+	tween = get_tree().create_tween()
+	#tween.set_parallel(true)
+	$Body.scale = Vector3(0.2, 1.0, 0.2)
+	tween.tween_property($Body, "scale", Vector3(1.0, 1.0, 1.0), 1.0).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
+	
+	#mesh.get("surface_0/material").
+	#tween.tween_property(mesh.get("surface_0/material"), "albedo_color", Color(1.0, 1.0, 1.0), 1.0)
+
 
 func die():
-	queue_free()
+	animation_player.queue("die")
 	
 
 func _on_detection_area_body_entered(body: Node3D) -> void:
@@ -62,4 +82,21 @@ func _on_shoot_timer_timeout() -> void:
 
 
 func _on_dmg_area_body_entered(body: Node3D) -> void:
-	pass
+	if body.is_in_group("Player"):
+	#	player.die()
+		body.take_damage(1)
+		player_touching = true
+		$ContactHitTimer.start()
+	
+
+
+func _on_dmg_area_body_exited(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+		player_touching = false
+
+
+func _on_contact_hit_timer_timeout() -> void:
+	if player_touching:
+		player.take_damage(1)
+		player_touching = true
+		$ContactHitTimer.start()
