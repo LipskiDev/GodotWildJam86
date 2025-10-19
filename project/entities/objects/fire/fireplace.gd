9c497a2d8f66e95f4dce00ab43d281824f8af280
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-10-19 21:44:28 +0200

    smaller fixes

--- project/entities/objects/fire/fireplace.gd
+++ project/entities/objects/fire/fireplace.gd
@@ -6,6 +6,7 @@ var found: bool = false
 
 @export var bonfire_id: int = -1
 
+
 func _ready() -> void:
 	$DmgTimer.wait_time = dmg_tick_speed
 	$HealTimer.wait_time = heal_tick_speed
@@ -21,8 +22,8 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	pass
-	
-	
+
+
 func _on_detection_area_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player") and !found:
 		print("new fireplace found")
@@ -33,8 +34,9 @@ func _on_detection_area_body_entered(body: Node3D) -> void:
 		$HealTimer.start()
 		Globals.last_bonfire_scene = get_tree().current_scene.scene_file_path
 
+
 func _on_damage_area_body_entered(body: Node3D) -> void:
-	if body.has_method("take_damage") and !found:
+	if body.has_method("take_damage"):
 		body.take_damage(1)
 		$DmgTimer.start()
 
@@ -44,10 +46,10 @@ func _on_dmg_timer_timeout() -> void:
 		if body.has_method("take_damage"):
 			body.take_damage(1)
 			$DmgTimer.start()
-			
+
 
 func _on_heal_timer_timeout() -> void:
-	for body in $DamageArea.get_overlapping_bodies():
+	for body in $DetectionArea.get_overlapping_bodies():
 		if body.is_in_group("Player"):
-			#body.heal(1)
+			body.heal(1)
 			$HealTimer.start()
--- project/ui/heart_management.gd
+++ project/ui/heart_management.gd
@@ -1,9 +1,6 @@
 extends HBoxContainer
 
 
-var health: int = 3
-
-
 @onready var heart: TextureRect = $Heart
 @onready var heart_2: TextureRect = $Heart2
 @onready var heart_3: TextureRect = $Heart3
@@ -15,7 +12,8 @@ func _ready() -> void:
 
 
 func _update() -> void:
-	health -= 1
+	var player: Player = get_tree().get_first_node_in_group("Player")
+	var health: int = player.health
 	heart.visible = health >= 1
 	heart_2.visible = health >= 2
 	heart_3.visible = health >= 3
